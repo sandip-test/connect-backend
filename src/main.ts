0bf0996e544a825c69febbfb3db4d8bd4bd906f3
Sangyog <sangyog.puri111@gmail.com>
2025-06-24 12:39:40 +0545

    Added swagger and config setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);  
+
+  const config = new DocumentBuilder()
+  .setTitle('Sayapatri API')
+  .setDescription('API documentation for Sayapatri API')
+  .setVersion('1.0')
+  .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  console.log(`API documentation available at: ${process.env.PORT}/api`);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
