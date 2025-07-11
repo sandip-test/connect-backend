7c45845f370072e2a4159f884a9cf8e4f03aae9a
Sangyog <sangyog.puri111@gmail.com>
2025-07-11 23:19:27 +0545

    Created(Admin seed): admin seed created

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AdminSeedService } from './modules/admin/seed/admin-seed.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // --- RUN THE ADMIN SEEDER ---
+  const adminSeeder = app.get(AdminSeedService);
+  await adminSeeder.seedAdmin();
+  // -----------------------------
 
   const config = new DocumentBuilder()
     .setTitle('Sayapatri API')
     .setDescription('API documentation for Sayapatri API')
     .setVersion('1.0')
+    .addBearerAuth() // Important for testing protected endpoints in Swagger
     .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
-  console.log(`Docs is available on http://localhost:${process.env.PORT}/api`);
-  await app.listen(process.env.PORT ?? 3000);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Docs is available on http://localhost:${port}/api`);
 }
 bootstrap();
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
+import { AdminSeedService } from '../admin/seed/admin-seed.service';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
-  exports: [UserService], 
+  imports: [
+    TypeOrmModule.forFeature([User]), 
+    ConfigModule 
+  ], 
+  providers: [
+    UserService, 
+    AdminSeedService 
+  ], 
+  exports: [
+    UserService, 
+    AdminSeedService 
+  ], 
 })
 export class UserModule {}
